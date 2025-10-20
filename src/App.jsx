8a52e1ca04704ea86ce3c7de5708dc97a69ac580
codeArt001 <olaniyanoluwajomiloju25@gmail.com>
2025-10-20 15:47:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <div className="dark:bg-black dark:text-white min-h-screen">
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "../assets/images/Logo-1.png";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center py-[5%] lg:w-[40%] w-[90%] m-auto mx-auto h-screen overflow-y-hidden">
+        <img src={logo} alt="" />
+        <p className="lg:text-6xl md:text-5xl text-4xl font-bold mt-3">
+          Welcome to VaultiQ
+        </p>
+        <p className="font-bold lg:text-2xl">Smart Control Stronger flow</p>
+        <p className="text-center mt-2 lg:text-sm text-xs">
+          Get instant insights into your spending, income, and hidden money
+          leaks all in one secure dashboard
+        </p>
+        <div className="mt-3 w-full">
+          <button className="rounded-2xl bg-blue-600 lg:text-[1.4rem]  py-2 mt-3 p-6 w-full hover:bg-blue-500">
+            Connect My Bank
+          </button>
+          <button className="rounded-2xl bg-blue-900 lg:text-[1.4rem]  py-2 mt-3 w-full hover:bg-blue-200 hover:text-black">
+            Continue Without Linking
+          </button>
+        </div>
+        <div className="mt-2  text-center">
+          <p>VaultiQ Never Stores Your Banking Credentials </p>
+          <p>Your Data Stays Encrypted and Private</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
